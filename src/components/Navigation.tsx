4d466cce1cf5d2c8e74f56294c6f48addcd96f2b
Matthew Miling <mattmiling@gmail.com>
2024-12-31 15:03:08 -0500

    Add front page image, fix portfolio button hover

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,21 @@
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/react';
+import { Menu, MenuButton, MenuItems, MenuItem, Disclosure, DisclosureButton } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/20/solid';
 
 export default function Navigation() {
     return (
         <>
-            <nav className="bg-black">
+            <Disclosure as="nav" className="bg-black">
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                     <div className="relative flex h-16 items-center justify-between">
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            {/* Mobile menu button*/}
+                            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <span className="absolute -inset-0.5" />
+                                <span className="sr-only">Open main menu</span>
+                                <Bars3Icon aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
+                                <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-[open]:block" />
+                            </DisclosureButton>
+                        </div>
                         <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                             <div className="hidden sm:ml-6 sm:block">
                                 <div className="flex space-x-4">
@@ -16,7 +26,7 @@ export default function Navigation() {
                         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                             <Menu as="div" className="ml-3 relative">
                                 <div className="flex space-x-4">
-                                    <MenuButton className="font-medium text-lg text-white hover:bg-gray-700" aria-expanded="false" aria-haspopup="true">
+                                    <MenuButton className="px-3 py-2 font-medium text-lg text-white hover:bg-gray-700" aria-expanded="false" aria-haspopup="true">
                                         portfolio
                                     </MenuButton>
                                 </div>
@@ -38,7 +48,7 @@ export default function Navigation() {
                         </div> 
                     </div>
                 </div>
-            </nav>
+            </Disclosure>
         </>
     )
 }
--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -2,7 +2,17 @@
 export default function Splash() {
     return (
         <>
-            <div className="bg-black">
+            <div className="relative bg-black">
+                <img src="/workstation.jpeg" alt="workstation" className="object-cover w-full h-96" />
+                <div className="absolute inset-0 flex items-center justify-center">
+                    { /*
+                        <h1 className="text-4xl font-bold text-white text-right text-shadow text-keep-newlines">
+                            Design
+                            Engineering
+                            Immersion
+                        </h1>
+                        */ }
+                </div>
             </div>
         </>
     );
